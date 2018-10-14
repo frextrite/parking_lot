1f5720f10dcb0f532568af437551209fac79f9cf
Amol Grover <29180487+frextrite@users.noreply.github.com>
2018-10-14 16:50:39 +0530

    Update test case for creating parking lot

--- src/app.py
+++ src/app.py
@@ -1,2 +1,5 @@
+lot = dict()
+
+
 def create_parking_lot(n):
     print(f"Created a parking lot with {n} slots")
--- test/test_parking_lot.py
+++ test/test_parking_lot.py
@@ -1,6 +1,6 @@
 import unittest
 
-from parking_lot.src.app import create_parking_lot
+from parking_lot.src.app import create_parking_lot, lot
 
 
 class ParkingLotTest(unittest.TestCase):
@@ -8,8 +8,15 @@ class ParkingLotTest(unittest.TestCase):
         pass
 
     def test_create_parking_lot(self):
+        expected_output = {'1': False,
+                           '2': False,
+                           '3': False,
+                           '4': False,
+                           '5': False,
+                           '6': False}
         slots = 6
         create_parking_lot(slots)
+        self.assertDictEqual(lot, expected_output)
 
 
 if __name__ == "__main__":
