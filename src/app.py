d7d744b64e84851d0ac4c3353b95c4df6c2c91fa
Amol Grover <29180487+frextrite@users.noreply.github.com>
2018-10-14 17:20:27 +0530

    Add parking method

--- src/app.py
+++ src/app.py
@@ -1,7 +1,14 @@
 lot = dict()
+R_NO_COLOR = dict()
+SLOT_NO_REG = dict()
+SLOT_NO_COLOR = dict()
 
 
 def create_parking_lot(n):
     for i in range(n):
         lot[i + 1] = False
     print(f"Created a parking lot with {n} slots")
+
+
+def park(registration_number, color):
+    pass
