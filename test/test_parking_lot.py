10b222d83eac634f69303a07ea572ccb65a2dbed
Amol Grover <29180487+frextrite@users.noreply.github.com>
2018-10-14 16:41:23 +0530

    Add test skeleton for creating a parking lot

--- test/test_parking_lot.py
+++ test/test_parking_lot.py
@@ -5,6 +5,10 @@ class ParkingLotTest(unittest.TestCase):
     def test_file(self):
         pass
 
+    def test_create_parking_lot(self):
+        slots = 6
+        create_parking_lot(slots)
+
 
 if __name__ == "__main__":
     unittest.main()
