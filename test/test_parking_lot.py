c1f01a51b3727157224c6cea17fd007a7dcae60b
Amol Grover <29180487+frextrite@users.noreply.github.com>
2018-10-14 18:37:49 +0530

    Add actual function call

--- test/test_parking_lot.py
+++ test/test_parking_lot.py
@@ -4,7 +4,7 @@ import unittest
 from io import StringIO
 from unittest.mock import patch
 
-from parking_lot.src.app import create_parking_lot, park, leave, status
+from parking_lot.src.app import create_parking_lot, park, leave, status, registration_numbers_for_cars_with_colour
 from parking_lot.src.app import DATA, LOT, R_NO_COLOR, SLOT_NO_REG, SLOT_NO_COLOR
 
 
@@ -76,6 +76,7 @@ class ParkingLotTest(unittest.TestCase):
     @patch.dict(R_NO_COLOR, {"White": ["KA-01-HH-1234", "KA-01-HH-9999"]}, clear=True)
     def test_registration_numbers_for_cars_with_colour(self):
         color = "White"
+        registration_numbers_for_cars_with_colour(color)
         self.assertEqual(sys.stdout.getvalue().strip(), "KA-01-HH-1234, KA-01-HH-9999")
 
 
