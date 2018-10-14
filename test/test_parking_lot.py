04ed61e2cd188197e1691ca76ea7d56a07d2704b
Amol Grover <29180487+frextrite@users.noreply.github.com>
2018-10-14 16:57:01 +0530

    Add ability to assert console output

--- test/test_parking_lot.py
+++ test/test_parking_lot.py
@@ -1,22 +1,30 @@
+import sys
 import unittest
+
+from io import StringIO
 
 from parking_lot.src.app import create_parking_lot, lot
 
 
 class ParkingLotTest(unittest.TestCase):
+    def setUp(self):
+        self.held, sys.stdout = sys.stdout, StringIO()
+
     def test_file(self):
         pass
 
     def test_create_parking_lot(self):
+        slots = 6
         expected_output = {1: False,
                            2: False,
                            3: False,
                            4: False,
                            5: False,
                            6: False}
-        slots = 6
+        expected_print = f"Created a parking lot with {slots} slots"
         create_parking_lot(slots)
         self.assertDictEqual(lot, expected_output)
+        self.assertEqual(sys.stdout.getvalue().strip(), expected_print)
 
 
 if __name__ == "__main__":
