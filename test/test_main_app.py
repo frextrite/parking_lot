4a42675cdf08193d1ee7e6ea5ea6df40e776dbb2
Amol Grover <29180487+frextrite@users.noreply.github.com>
2018-10-14 19:54:16 +0530

    Add test case for checking status of parked cars from CLI

--- test/test_main_app.py
+++ test/test_main_app.py
@@ -44,6 +44,15 @@ class MainAppTest(unittest.TestCase):
         cli.onecmd("leave 4")
         self.assertEqual(sys.stdout.getvalue().strip(), "Slot number 4 is free")
 
+    @patch.dict(LOT, {1: False, 2: True, 3: False, 4: True}, clear=True)
+    @patch.dict(DATA, {2: {'registration_number': "KA-01-HH-1234", 'color': "White"}, 4: {'registration_number': "KA-01-HH-9999", 'color': "White"}}, clear=True)
+    @patch.dict(R_NO_COLOR, {"White": ["KA-01-HH-1234", "KA-01-HH-9999"]}, clear=True)
+    @patch.dict(SLOT_NO_REG, {"KA-01-HH-1234": 2, "KA-01-HH-9999": 4}, clear=True)
+    @patch.dict(SLOT_NO_COLOR, {"White": [2, 4]}, clear=True)
+    def test_status(self):
+        cli = self.create()
+        cli.onecmd("status")
+        self.assertEqual(sys.stdout.getvalue().strip(), "Slot No.\tRegistration No.\tColor\n2\tKA-01-HH-1234\tWhite\n4\tKA-01-HH-9999\tWhite")
 
 if __name__ == "__main__":
     unittest.main()
