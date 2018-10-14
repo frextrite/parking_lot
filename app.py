af3783fb7843caf8bcfeb5839b2c4d73acc5a0a7
Amol Grover <29180487+frextrite@users.noreply.github.com>
2018-10-14 18:57:46 +0530

    Add main runner file

---
+++ app.py
@@ -0,0 +1,8 @@
+from parking_lot.src.app import (create_parking_lot, 
+                                 park,
+                                 leave,
+                                 status,
+                                 registration_numbers_for_cars_with_colour,
+                                 slot_numbers_for_cars_with_colour,
+                                 slot_number_for_registration_number_exists)
+from parking_lot.src.app import DATA, LOT, R_NO_COLOR, SLOT_NO_REG, SLOT_NO_COLOR
